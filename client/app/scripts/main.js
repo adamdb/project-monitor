8262093d1e717c27dc6355b20f85c50c6df985cf
Adam Balchunas <adam.balchunas@digitas.com>
2014-08-01 17:20:15 -0400

    Refactored project structure and compiling files included by require.js for production

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -2,11 +2,11 @@
 
 require.config({
   paths: {
-    jquery: '../bower_components/jquery/dist/jquery',
-    underscore: '../bower_components/underscore/underscore',
-    backbone: '../bower_components/backbone/backbone',
-    text: '../bower_components/text/text',
-    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap.min'
+    jquery: '../../bower_components/jquery/dist/jquery',
+    underscore: '../../bower_components/underscore/underscore',
+    backbone: '../../bower_components/backbone/backbone',
+    text: '../../bower_components/text/text',
+    bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
     'bootstrap': {
