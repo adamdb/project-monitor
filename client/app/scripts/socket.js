d5d5b32b706b62b0146f42f1bc38b297d80001f3
Adam Balchunas <adam.balchunas@digitas.com>
2014-08-05 13:56:00 -0400

    Debugging

--- client/app/scripts/socket.js
+++ client/app/scripts/socket.js
@@ -6,12 +6,15 @@ define(['socketioclient', './models/model'], function(io, model) {
       console.log('connected');
     
       socket.on(model.BUILD_STARTED, function(data) {
+        console.log(data);
       });
 
       socket.on(model.BUILD_COMPLETED, function(data) {
+        console.log(data);
       });
 
       socket.on(model.BUILD_FINALIZED, function(data) {
+        console.log(data);
       });
     });
   }
