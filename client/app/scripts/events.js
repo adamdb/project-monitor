864737fa09bbe5c3fe75c87fab9d7e85c30d36e0
Adam Balchunas <adam.balchunas@digitas.com>
2014-08-06 15:47:36 -0400

    Debugging

--- client/app/scripts/events.js
+++ client/app/scripts/events.js
@@ -4,7 +4,7 @@ define(['backbone', 'underscore'], function(Backbone, _) {
   var events = function events() {
     this.listener = {};
     _.extend(this.listener, Backbone.Events);
-  }();
+  };
 
   return events;
 });
