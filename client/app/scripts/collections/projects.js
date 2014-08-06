78a0c302868965531aa07d7d958e59c7dea15c2e
Adam Balchunas <adam.balchunas@digitas.com>
2014-08-06 13:17:23 -0400

    Added methods to add and retrieve data from MongoDB

--- client/app/scripts/collections/projects.js
+++ client/app/scripts/collections/projects.js
@@ -8,7 +8,16 @@ define([
 ], function(_, Backbone, ProjectModel) {
   var Collection = Backbone.Collection.extend({
     url: '/projects',
-    model: ProjectModel
+    model: ProjectModel,
+    
+    initialize: function initialize() {
+      this.fetch();
+    },
+
+    parse: function parse(res) {
+      console.log('have data=' + res.data);
+      return res.data;
+    };
   });
 
   return Collection;
