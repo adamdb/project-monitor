ef8c93fec74aeff25b5979d333f46cc9dae620fc
Adam Balchunas <adam.balchunas@digitas.com>
2014-09-22 13:55:14 -0400

    Refactored

---
+++ client/app/monitor.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var angular = require('angular');
+
+var ListController = require('./list/ListController');
+
+var app = angular.module('project-monitor', []);
+app.controller('ListController', ['$scope', ListController]);
+
+app.directive('listItem', listItem);
